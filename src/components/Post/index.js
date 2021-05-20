b26dfec36fc90970f3369d01b50b1bbc40a1d69c
Pratham Meghani <premmeghani16@gmail.com>
2021-05-20 20:06:08 +0530

    PlayPauseImplemented

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,16 +1,25 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React,{useState} from 'react'
+import { StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native'
 import Video from 'react-native-video'
 import styles from './styles'
 const Post = () => {
+
+    const [paused,setPaused]=useState(false);
+    const onplayPausePress=()=>{
+setPaused(!paused)
+    }
     return (
         <View style={styles.container}>
+            <TouchableWithoutFeedback onPress={onplayPausePress}>
            <Video
            source={{uri:'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4'}}
            style={styles.video}
            onError={(e)=>console.log(e)}
            resizeMode='cover'
+           repeat={true}
+           paused={paused}
            />
+           </TouchableWithoutFeedback>
         </View>
     )
 }
